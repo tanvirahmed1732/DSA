f8ded8004eb7f766c139e01360dde2ef650fa2ab
Tanvir Ahmed <tanvirahmed17341@gmail.com>
2024-07-29 20:49:55 +0600

    9

---
+++ Strings/Longest Repeating Character Replacement.py
@@ -0,0 +1,18 @@
+#two pointer approach
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        ans=0
+        freq={}
+        l=0
+        for r in range(len(s)):
+            if s[r] in freq: #collecting frequency of the char from the string until r position
+                freq[s[r]]+=1
+            else:
+                freq[s[r]]=1
+            lent=r-l+1 #distance between two pointer
+            if lent-max(freq.values())<=k: #checking how many different char available in the lent.
+                ans=max(ans,lent) # that means, if the other char is less than k from the max freq char then update the ans
+            else:
+                freq[s[l]]-=1 #else move the left pointer and update the freq dictionary.
+                l+=1
+        return ans
